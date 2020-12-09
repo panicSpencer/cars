012359e5e9aadd8e331befb669cd3968afc124f2
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-12-08 22:17:35 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -4,11 +4,12 @@
 
 while [ "$num" -ne "3" ]
 do
+  
   echo "Type 1 to enter a new car, type 2 to display the list of cars, or type 3 to quit and exit the program"
 	case $num in
-	"1")
-		echo "Year?" ;;
+	"1") echo "Year?" ;;
 	"2") echo "Hello" ;;
 	"3") echo "Thank you goodbye" ;;
 	*) echo "Sorry, that is not a valid number" ;;
+	esac
 done
