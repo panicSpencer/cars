59e166f5a2c6d3049fe21724ff269da92f9e8e81
panicSpencer <72724866+panicSpencer@users.noreply.github.com>
2020-12-08 23:00:53 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -5,11 +5,19 @@
 num=0
 while [ "$num" -ne "3" ]
 do
-  
   echo "Type 1 to enter a new car, type 2 to display the list of cars, or type 3 to quit and exit the program"
 	read num
 	case $num in
-	"1") echo "Year?" ;;
+	"1") 
+		echo "Year?" 
+		read year
+		echo "Make?"
+		read make
+		echo "Model
+		read model
+		newcar="$year:$make:$model" ;;
+		
+		
 	"2") echo "Hello" ;;
 	"3") echo "Thank you goodbye" ;;
 	*) echo "Sorry, that is not a valid number" ;;
